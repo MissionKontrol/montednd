30b768ad3d61ed73dbbea387cce4495d60472bcb
Peter Forsythe <peter.forsythe@albelli.com>
2021-09-19 20:27:08 -0400

    copy stats everywhere

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use rand::Rng;
 
-#[derive(Default)]
+#[derive(Default, Debug, Clone)]
 struct CharacterStruct {
     name: String,
+    stats: CharacterCapabilities,
+}
+
+#[derive(Default, Debug, Copy, Clone)]
+struct CharacterCapabilities {
     hit_points: u8,
     armour_class: u8,
     to_hit: u8,
@@ -19,10 +24,13 @@ fn battle( players: &Vec<CharacterStruct>, num_turns: u8) {
     let mut rng = rand::thread_rng();
     let turn_order: [bool;10]= [rand::random(); 10];
 
+    let p1 = players[0].stats;
+    let p2 = players[1].stats;
+
     for i in 0..num_turns {
-        let attack_result = melee_attack(players[0].to_hit, players[1].armour_class, players[0].damage);
+        let attack_result = melee_attack(p1.to_hit, p2.armour_class, p1.damage);
         println!("{} Attacks with {} for {} damage.",players[0].name,attack_result.attack_roll, attack_result.damage_roll);        
-        let attack_result = melee_attack(players[1].to_hit, players[0].armour_class, players[0].damage);
+        let attack_result = melee_attack(p1.to_hit, p2.armour_class, p1.damage);
         println!("{} Attacks with {} for {} damage.",players[1].name,attack_result.attack_roll, attack_result.damage_roll);        
     }    
 }
@@ -47,22 +55,22 @@ fn melee_attack(to_hit: u8, armour_class: u8, damage: u8) -> attack_result {
 }
 
 fn main() {
-    let player1 = CharacterStruct {
-        name: String::from("Player 1"),
+    let pstats = CharacterCapabilities{
         hit_points: 10,
         armour_class: 1,
         to_hit: 12,
         actions_per_round: 1,
-        damage: 6,
+        damage: 6,        
+    };
+
+    let player1 = CharacterStruct {
+        name: String::from("Player 1"),
+        stats: pstats,
     };
 
     let player2 = CharacterStruct {
         name: String::from("Player 2"),
-        hit_points: 10,
-        armour_class: 1,
-        to_hit: 12,
-        actions_per_round: 1,
-        damage: 6,
+        stats: pstats,
     };
 
     let mut player_vec = Vec::new(); 
