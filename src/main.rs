1ba989945c39e75599571f92f6a6fce0f6d5427f
Peter Forsythe <peter.forsythe@albelli.com>
2021-09-21 18:08:16 -0400

    clone and pass everything

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,6 @@ use rand::Rng;
 #[derive(Default, Debug, Clone)]
 struct CharacterStruct {
     name: String,
-    stats: CharacterCapabilities,
-}
-
-#[derive(Default, Debug, Copy, Clone)]
-struct CharacterCapabilities {
     hit_points: u8,
     armour_class: u8,
     to_hit: u8,
@@ -18,6 +13,7 @@ struct CharacterCapabilities {
 struct AttackResult {
     attack_roll: u8,
     damage_roll: u8,
+    attack_result: ActionResult,
 }
 
 struct DamageResult{
@@ -32,37 +28,103 @@ enum HealthState{
     NOMINAL,
 }
 
-fn battle( players: &Vec<CharacterStruct>, num_turns: u8) {
-    let mut rng = rand::thread_rng();
-    let turn_order: [bool;10]= [rand::random(); 10];
-
-    let mut p1 = players[0].stats;
-    let mut p2 = players[1].stats;
-
-    for i in 0..num_turns {
-        let attack_result = melee_attack(p1.to_hit, p2.armour_class, p1.damage);
-        println!("{} Attacks with {} for {} damage.",players[0].name,attack_result.attack_roll, attack_result.damage_roll);        
-        let damage_done = resolve_damage(attack_result.damage_roll, p2.hit_points);
-        println!("  {:?} is {:?}  remaining hit point: {:?}", players[1].name, damage_done.target_state, damage_done.remaining_hit_points);
-        match damage_done.target_state {
-            HealthState::DEAD => break,
-            HealthState::KO => break,
-            _=>p2.hit_points=damage_done.remaining_hit_points,
-        };
-
-        let attack_result = melee_attack(p1.to_hit, p2.armour_class, p1.damage);
-        println!("{} Attacks with {} for {} damage.",players[1].name,attack_result.attack_roll, attack_result.damage_roll);        
-        let damage_done = resolve_damage(attack_result.damage_roll, p1.hit_points);
-        println!("  {:?} is {:?}  remaining hit point: {:?}", players[0].name, damage_done.target_state, damage_done.remaining_hit_points);
-        match damage_done.target_state {
-            HealthState::DEAD => break,
-            HealthState::KO => break,
-            _=>p1.hit_points=damage_done.remaining_hit_points,
-        };
-    }    
+#[derive(Default)]
+struct TurnResult {
+    winner: CharacterStruct,
+    battle_data: Vec<BattleData>,
 }
 
-fn battle_turn(){
+struct BattleData {
+    actor: String,
+    action_number: u16,
+    action_type: ActionType,
+    action_roll: u8,
+    action_result: ActionResult,
+    action_damage: u16
+}
+
+#[derive(Default, Clone)]
+struct BattleOrder {
+    initative_roll: u8,
+    character: CharacterStruct,
+}
+
+
+enum ActionResult {
+    CritFail,
+    Fail,
+    Miss,
+    Hit,
+    CritHit,
+}
+
+enum ActionType {
+    Attack,
+    Dodge,
+    Cast,
+    Initative,
+}
+
+fn battle( players: &Vec<CharacterStruct>, num_turns: u8) {
+    let battle_order = make_battle_order(players);
+    for i in 1..num_turns {
+        run_battle_turn(battle_order.clone());
+    }   
+}
+
+fn make_battle_order(players: &Vec<CharacterStruct>) -> Vec<BattleOrder> {
+    let mut rng = rand::thread_rng();
+    let mut battle_order = Vec::new();
+
+    for player in players {
+        let player_order = BattleOrder {
+            character: player.clone(),
+            initative_roll: rng.gen_range(1..20),
+        };
+        battle_order.push(player_order);
+    }
+    battle_order
+}
+
+fn run_battle_turn(mut battle_order: Vec<BattleOrder>) -> TurnResult{
+    let mut turn_result: TurnResult = Default::default();
+
+    let x = battle_order.clone();
+    battle_order.sort_by(|a, b| b.initative_roll.cmp(&a.initative_roll));
+    let mut i = 1;
+    for action in battle_order {
+        if action.character.hit_points > 0 {
+            let attack_target = action.select_target(x.clone());
+            let attack_result = melee_attack(action.character.to_hit, attack_target.character.armour_class, action.character.damage);
+            println!("{} Attacks with {} for {} damage.",action.character.name, attack_result.attack_roll, attack_result.damage_roll);        
+            let damage_done = resolve_damage(attack_result.damage_roll, attack_target.character.hit_points);
+            println!("  {:?} is {:?}  remaining hit point: {:?}", attack_target.character.name, damage_done.target_state, damage_done.remaining_hit_points);
+
+            let battle_data = BattleData {
+                actor: action.character.name,
+                action_number: i,
+                action_type: ActionType::Attack,
+                action_roll: attack_result.attack_roll,
+                action_result: attack_result.attack_result,
+                action_damage: attack_result.damage_roll as u16,
+            };
+            i += 1;
+            turn_result.battle_data.push(battle_data);
+        }
+        else {break}
+    }
+    turn_result
+}
+
+impl BattleOrder { 
+    fn select_target(&self, battle_order: Vec<BattleOrder>) -> BattleOrder {
+        for player in battle_order {
+            if player.character.name != self.character.name {
+                return player.clone();
+            }
+        }
+        panic!("failed to find target");
+    }
 }
 
 fn resolve_damage(damage: u8, hit_points: u8) -> DamageResult {
@@ -84,32 +146,34 @@ fn melee_attack(to_hit: u8, armour_class: u8, damage: u8) -> AttackResult {
     let mut result = AttackResult{
         attack_roll: 0,
         damage_roll: 0,
+        attack_result: ActionResult::Miss,
     };
     
     result.attack_roll  = rng.gen_range(1..to_hit);
     if  result.attack_roll > armour_class {
         result.damage_roll = rng.gen_range(1..damage);
+        result.attack_result = ActionResult::Hit;
     }
     result
 }
 
 fn main() {
-    let pstats = CharacterCapabilities{
+    let player1 = CharacterStruct {
+        name: String::from("Player 1"),
         hit_points: 10,
         armour_class: 1,
         to_hit: 12,
         actions_per_round: 1,
-        damage: 6,        
-    };
-
-    let player1 = CharacterStruct {
-        name: String::from("Player 1"),
-        stats: pstats,
+        damage: 6,  
     };
 
     let player2 = CharacterStruct {
         name: String::from("Player 2"),
-        stats: pstats,
+        hit_points: 10,
+        armour_class: 1,
+        to_hit: 12,
+        actions_per_round: 1,
+        damage: 6,  
     };
 
     let mut player_vec = Vec::new(); 
