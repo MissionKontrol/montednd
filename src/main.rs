c82f0fc9fa794efd7ae98533e9ee995fa44f7c6a
Peter Forsythe <peter.forsythe@albelli.com>
2021-09-28 20:01:48 -0400

    code re-org

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::thread;
+use std::fmt::Write;
 use rand::Rng;
 
 fn main() {
@@ -42,26 +43,26 @@ fn main() {
     let player_vec3 = player_vec.clone();
     let player_vec4 = player_vec.clone();
 
-    let thread_iterations = 1_000_000;
+    let thread_iterations = 250_000;
 
     let thread_one = thread::spawn(move||
         {
-            battle(&player_vec1, thread_iterations)
+            battle(&player_vec1, thread_iterations, "1")
         });
         
     let thread_two = thread::spawn(move||
         {
-            battle(&player_vec2, thread_iterations)
+            battle(&player_vec2, thread_iterations, "2")
         });
 
     let thread_three = thread::spawn(move||
         {
-            battle(&player_vec3, thread_iterations)
+            battle(&player_vec3, thread_iterations, "3")
         });    
 
     let thread_four = thread::spawn(move||
         {
-            battle(&player_vec4, thread_iterations)
+            battle(&player_vec4, thread_iterations, "4")
         });   
 
     let _res = thread_one.join();
@@ -72,7 +73,7 @@ fn main() {
 }
 
 
-#[derive(Default, Debug, Clone)]
+#[derive(Default, Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 struct CharacterStruct {
     name: String,
     hit_points: u8,
@@ -81,6 +82,17 @@ struct CharacterStruct {
     actions_per_round: u8,
     damage: u8,
     team: Team,
+}
+
+impl CharacterStruct {
+    fn select_target(&self, combatant_list: &Vec<BattleOrder>) -> Option<usize> {
+        for i in  0..combatant_list.len() {
+            if combatant_list[i].character.team != self.team && combatant_list[i].character_state == HealthState::NOMINAL {
+                return Some(i);
+            }
+        }
+        None
+    }
 }
 
 struct AttackResult {
@@ -95,7 +107,7 @@ struct DamageResult{
     target_state: HealthState,
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialOrd, Eq, Ord, PartialEq)]
 enum HealthState{
     DEAD,
     KO,
@@ -134,150 +146,34 @@ struct BattleResult{
     turns_run: u8,
     winner: CharacterStruct,
     turn_result: Vec<TurnResult>,
-
-    // battle_order: BattleOrder,
+    battle_order_list: Vec<BattleOrder>,
 }
 
-#[derive(Default, Debug, Clone)]
+impl BattleResult {
+    fn summarize_battle(&self, prefix: &str ) {
+        let mut output: String;
+
+        output = format!("{}-{} {} {}", prefix, 
+            self.battle_id.to_string(), 
+            self.turns_run.to_string(), 
+            self.winner.name.to_string());
+
+        let initiative_winner = self.battle_order_list.iter().max_by_key(|p| p.initative_roll);
+        match initiative_winner {
+            Some(winner) => write!(output, " {} ", winner.character.name.to_string()).unwrap(),
+            None => write!(output, "initative Tie I guess").unwrap(),
+        }
+
+        println!("{}", output);
+    }
+}
+
+#[derive(Default, Debug, Eq, Ord, PartialEq, PartialOrd, Clone)]
 struct BattleOrder {
     initative_roll: u8,
     character_state: HealthState,
     character: CharacterStruct,
     team: Team,
-}
-
-#[derive(Clone, PartialEq, Debug)]
-enum Team {
-    Heros,
-    Villains,
-}
-
-impl Default for Team {
-    fn default() -> Self {
-        Team::Heros
-    }
-}
-
-#[derive(Clone, Debug)]
-enum ActionResultType {
-    _CritFail,
-    _Fail,
-    Miss,
-    Hit,
-    _CritHit,
-}
-
-#[derive(Clone, Debug)]
-enum ActionType {
-    Attack,
-    _Dodge,
-    _Cast,
-    _Initative,
-}
-
-fn battle( players: &Vec<CharacterStruct>, num_iterations: u32) {
-    let battle_order = make_battle_order(players);
-    let mut battle_collection:Vec<BattleResult> = Vec::new();
-
-    for battle_num in 0..num_iterations {
-        let mut battle_result = run_battle(battle_order.clone());
-        battle_result.battle_id = battle_num;
-        battle_collection.push(battle_result.clone());
-    }
-    for battle in battle_collection {
-        println!("Battle: {}  {} turns won by {:?} {}",battle.battle_id, battle.turns_run, battle.winner.name, battle.winner.hit_points);
-        // for turn in battle.turn_result {
-        //     for action in turn.action_results {
-        //         println!("  {} {} {} {:?} {} {:?} {} {:?}",
-        //             turn.turn_number,
-        //             action.action_number,    
-        //             action.actor,
-        //             action.action_type, action.target, action.action_result, action.action_damage, action.damage_done.target_state);
-        //     }
-        // }
-    }
-
-}
-
-fn make_battle_order(players: &Vec<CharacterStruct>) -> Vec<BattleOrder> {
-    let mut rng = rand::thread_rng();
-    let mut battle_order = Vec::new();
-    
-    for player in players {
-        let player_order = player.clone();
-        let initative_roll = rng.gen_range(1..21);
-        println!("{} {}", player.name, initative_roll);
-        let order = BattleOrder {
-            initative_roll: initative_roll,
-            character: player_order,
-            character_state: HealthState::NOMINAL,
-            team: player.team.clone(),
-        };
-        battle_order.push(order);
-    }
-    battle_order.sort_by(|a,b| b.initative_roll.cmp(&a.initative_roll) );
-    battle_order
-}
-
-fn run_battle(mut battle_order: Vec<BattleOrder>) -> BattleResult {
-    let mut battle_result: BattleResult = Default::default();
-    let mut winning_result = false;
-    let mut turn_count: u8 = 0;
-
-    while !winning_result {
-        let turn_result = run_battle_turn(&mut battle_order, turn_count);
-        turn_count +=1;
-        match turn_result {
-            Some(x) => battle_result.turn_result.push(x),
-            None => winning_result=true,
-        }
-    }
-    let winner = get_winner(battle_order).unwrap();
-    battle_result.turns_run = turn_count-1;
-    battle_result.winner = winner.character;
-    battle_result
-}
-
-fn get_winner(battle_order: Vec<BattleOrder>) -> Option<BattleOrder> {
-    for player in battle_order {
-        if player.character_state == HealthState::NOMINAL {
-            return Some(player);
-        }
-    }
-    None
-}
-
-fn run_battle_turn(battle_order: &mut Vec<BattleOrder>, turn_number: u8) -> Option<TurnResult>{
-    let mut turn_result: TurnResult = Default::default();
-    turn_result.turn_number = turn_number;
-
-    for i in 0..battle_order.len() {
-        if battle_order[i].character_state == HealthState::NOMINAL {
-            let target = battle_order[i].character.select_target(&battle_order);
-            match target {
-                Some(target) => {
-                    let attack_result = melee_attack(battle_order[i].character.to_hit, battle_order[target].character.armour_class, battle_order[i].character.damage);
-                    let damage_done = battle_order[target].resolve_damage(attack_result.damage_roll);
-                
-                    let action_result =  ActionResult {
-                        actor: battle_order[i].character.name.clone(),
-                        target: battle_order[target].character.name.clone(),
-                        action_number: i as u16,                           
-                        action_type: ActionType::Attack,
-                        action_roll: attack_result.attack_roll,
-                        action_result: attack_result.attack_result,
-                        action_damage: attack_result.damage_roll as u16,
-                        damage_done: damage_done,
-                    };
-                    turn_result.action_results.push(action_result);
-                }
-                None => {
-                    return Option::None}
-            }
-
-        }
-    }
-    Some(turn_result)
 }
 
 impl BattleOrder {
@@ -308,15 +204,140 @@ impl BattleOrder {
 
 }
 
-impl CharacterStruct {
-    fn select_target(&self, combatant_list: &Vec<BattleOrder>) -> Option<usize> {
-        for i in  0..combatant_list.len() {
-            if combatant_list[i].character.team != self.team && combatant_list[i].character_state == HealthState::NOMINAL {
-                return Some(i);
-            }
-        }
-        None
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+enum Team {
+    Heros,
+    Villains,
+}
+
+impl Default for Team {
+    fn default() -> Self {
+        Team::Heros
     }
+}
+
+#[derive(Clone, Debug)]
+enum ActionResultType {
+    _CritFail,
+    _Fail,
+    Miss,
+    Hit,
+    _CritHit,
+}
+
+#[derive(Clone, Debug)]
+enum ActionType {
+    Attack,
+    _Dodge,
+    _Cast,
+    _Initative,
+}
+
+fn battle( players: &Vec<CharacterStruct>, num_iterations: u32, battle_prefix: &str) {
+    let battle_order_list = make_battle_order_list(players);
+    let mut battle_collection_list:Vec<BattleResult> = Vec::new();
+
+    for battle_num in 0..num_iterations {
+        let mut battle_result = run_battle(battle_order_list.clone());
+        battle_result.battle_id = battle_num;
+        battle_result.battle_order_list = battle_order_list.clone();
+        battle_collection_list.push(battle_result.clone());
+    }
+    for battle in battle_collection_list {
+        battle.summarize_battle(battle_prefix);
+        // println!("Battle: {}  {} turns won by {:?} {}",battle.battle_id, battle.turns_run, battle.winner.name, battle.winner.hit_points);
+        // for turn in battle.turn_result {
+        //     for action in turn.action_results {
+        //         println!("  {} {} {} {:?} {} {:?} {} {:?}",
+        //             turn.turn_number,
+        //             action.action_number,    
+        //             action.actor,
+        //             action.action_type, action.target, action.action_result, action.action_damage, action.damage_done.target_state);
+        //     }
+        // }
+    }
+
+}
+
+fn make_battle_order_list(players: &Vec<CharacterStruct>) -> Vec<BattleOrder> {
+    let mut rng = rand::thread_rng();
+    let mut battle_order_list = Vec::new();
+    
+    for player in players {
+        let player_order = player.clone();
+        let initative_roll = rng.gen_range(1..21);
+        println!("{} {}", player.name, initative_roll);
+        let order = BattleOrder {
+            initative_roll: initative_roll,
+            character: player.clone(),
+            character_state: HealthState::NOMINAL,
+            team: player.team.clone(),
+        };
+        battle_order_list.push(order);
+    }
+    battle_order_list.sort_by(|a,b| b.initative_roll.cmp(&a.initative_roll) );
+    battle_order_list
+}
+
+fn run_battle(mut battle_order_list: Vec<BattleOrder>) -> BattleResult {
+    let mut battle_result: BattleResult = Default::default();
+    let mut winning_result = false;
+    let mut turn_count: u8 = 0;
+
+    while !winning_result {
+        let turn_result = run_battle_turn(&mut battle_order_list, turn_count);
+        turn_count +=1;
+        match turn_result {
+            Some(x) => battle_result.turn_result.push(x),
+            None => winning_result=true,
+        }
+    }
+    let winner = get_winner(battle_order_list).unwrap();
+    battle_result.turns_run = turn_count-1;
+    battle_result.winner = winner.character;
+    battle_result
+}
+
+fn get_winner(battle_order_list: Vec<BattleOrder>) -> Option<BattleOrder> {
+    for player in battle_order_list {
+        if player.character_state == HealthState::NOMINAL {
+            return Some(player);
+        }
+    }
+    None
+}
+
+fn run_battle_turn(battle_order_list: &mut Vec<BattleOrder>, turn_number: u8) -> Option<TurnResult>{
+    let mut turn_result: TurnResult = Default::default();
+    turn_result.turn_number = turn_number;
+
+    for i in 0..battle_order_list.len() {
+        if battle_order_list[i].character_state == HealthState::NOMINAL {
+            let target = battle_order_list[i].character.select_target(&battle_order_list);
+            match target {
+                Some(target) => {
+                    let attack_result = melee_attack(battle_order_list[i].character.to_hit, battle_order_list[target].character.armour_class, battle_order_list[i].character.damage);
+                    let damage_done = battle_order_list[target].resolve_damage(attack_result.damage_roll);
+                
+                    let action_result =  ActionResult {
+                        actor: battle_order_list[i].character.name.clone(),
+                        target: battle_order_list[target].character.name.clone(),
+                        action_number: i as u16,                           
+                        action_type: ActionType::Attack,
+                        action_roll: attack_result.attack_roll,
+                        action_result: attack_result.attack_result,
+                        action_damage: attack_result.damage_roll as u16,
+                        damage_done: damage_done,
+                    };
+                    turn_result.action_results.push(action_result);
+                }
+                None => {
+                    return Option::None}
+            }
+
+        }
+    }
+    Some(turn_result)
 }
 
 fn melee_attack(to_hit: u8, armour_class: u8, damage: u8) -> AttackResult {
